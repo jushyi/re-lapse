ea780d624fe98f9612495f0b7c2baa29f22db389
jushyi <maserinj@gmail.com>
2026-01-25 11:00:16 -0500

    refactor(28-04): extract FeedPhotoCard styles
    - Created FeedPhotoCard.styles.js (98 lines)
    - Reduced FeedPhotoCard.js from 203 to 105 lines
    - Added to styles barrel export

--- src/styles/index.js
+++ src/styles/index.js
@@ -21,3 +21,4 @@
 export { styles as swipeablePhotoCardStyles } from './SwipeablePhotoCard.styles';
 export { styles as cameraScreenStyles } from './CameraScreen.styles';
 export { styles as darkroomScreenStyles } from './DarkroomScreen.styles';
+export { styles as feedPhotoCardStyles } from './FeedPhotoCard.styles';
